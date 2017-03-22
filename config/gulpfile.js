a322086997eb4c19b5d03d673106f54a71091c69
Javier Villanueva <javier@medialounge.co.uk>
2017-03-22 12:25:10 +0000

    Use guetzli to improve jpg image compression

--- config/gulpfile.js
+++ config/gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
+var imageminGuetzli = require('imagemin-guetzli');
 
 gulp.task('default', () =>
     gulp.src(['src/**/*', '!src/optimised/**'])
-        .pipe(imagemin())
+        .pipe(imagemin([
+            imageminGuetzli(),
+            imagemin.optipng(),
+            imagemin.gifsicle(),
+            imagemin.svgo()
+        ]))
         .pipe(gulp.dest('src/optimised'))
 );
